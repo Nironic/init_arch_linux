d8b383aa58848314e77c69449736d1a480b1f773
Nironic <40025622+Nironic@users.noreply.github.com>
2024-12-23 15:11:30 +0000

    Add files via upload

--- install.py
+++ install.py
@@ -1,137 +1,79 @@
 import os
-red = "\033[91m"
-green = "\033[92m"
-blue  = "\033[94m"
 
-def run(com):
-    res = os.system(com)
-    return res
+def __install__():
+    os.system("sudo pacman -Syuu --noconfirm")
+    os.system("sudo pacman -Syuu python python-pip --noconfirm")
+    os.system("pip install colorama --break-system-packages")
 
-def log(text, info):
-    if info == "i":
-        print(blue + "[INFO]: " + text + "\033[0m")
-    if info == "c":
-        print(green + "[COMPLETE]: " + text + "\033[0m")
-    if info == "e":
-        print(red + "[ERROR]: " + text + "\033[0m")
+def __install__yay__():
+    os.system("sudo pacman -S --needed base-devel git --noconfirm")
+    os.system("cd yay && makepkg -si --noconfirm")
+    os.system("sudo yay -Syuu --noconfirm")
 
 def install(pack):
-    for i in pack:
-        log("Установка " + i + "..", "i")
-        res = run("sudo pacman -S " + i)
-        if res == 0:
-            log("Установка пакета " + i + " прошла успешно.", "c")
-        else:
-            log("Установка пакета " + i + " завершена с ошибкой.", "e")
+    os.system("sudo yay -Syuu --noconfirm " + pack)
 
-def KDE(coms):
-    error = 0
-    for i in coms:
-        res = run(i)
-        if res != 0:
-            error += 1
-    return error
 
-def Gchrome():
-    log("Установка AUR для загрузки браузера.")
-    install(["base-devel", "git"])
-    log("Установка компонентов браузера Google Chrome.", "i")
-    res = KDE(["git clone https://aur.archlinux.org/yay-git.git", "cd yay-git && makepkg -si"])
-    if res == 0:
-        log("Установка компонентов успешно завершена.", "c")
-    else:
-        log("Установка Google Chrome компонентов произошла ошибка.", "e")
-        break
-    log("Установка Google Chrome..", "i")
-    res = run("yay -S google-chrome")
-    if res == 0:
-        log("Google Chrome успешно установлен.", "c")
-    else:
-        log("Google Chrome не установлен.", "e")
+packet = ["ananicy", "google-chrome", "portproton", "visual-studio-code-bin", "mesa", "lib32-mesa", "vulkan-radeon", "lib32-vulkan-radeon", "vulkan-icd-loader", "lib32-vulkan-icd-loader", "vlc", "ardour", "telegram-desktop-bin", "steam", "bluez", "bluez-deprecated-tools", "shadowsocks"]
 
-def teleinstall():
-    log("Установка Telegramm.", "i")
-    res = run("sudo pacman -S telegram-desktop")
-    if res == 0:
-        log("Telegramm успешно установлен.")
-    else:
-        log("В установке Telegramm произошла ошибка.", "e")
-
-def setting_pip():
-    log("Настройка pip.", "i")
-    user = os.getlogin()
-    run("mkdir /home/" + str(user) + "/pip")
-    file = open("/home/" + user + ".config/pip/pip.conf", "w")
-    file.write("[global]\nbreak-system-packages = true")
-    file.close()
-    log("Pip успешно настроен", "c")
-
-def pacmans():
-    log("Настройка Pacman.", "i")
-    file = open("/etc/pacman.conf", "r")
-    info = file.read()
-    file.close()
-    info = info.replace("#ParallelDownloads = 5", "ParallelDownloads = 10")
-    file = open("etc/pacman.conf", "w")
-    file.write(info)
-    file.close()
-    log("Pacman успешно настроен.", "c")
-
-def optimizate():
-    log("Установка пакетов для оптимизации системы. [Шаг 1]", "i")
-    error = KDE(["git clone https://aur.archlinux.org/ananicy.git", "cd ananicy && makepkg -sric", "sudo systemctl enable --now ananicy"])
-    if error == 0:
-        log("[Шаг 1]: Успешно", "c")
-    else:
-        log("[Шаг 1]: Произошла ошибка", "e")
-    log("Установка пакетов для оптимизации системы. [Шаг 2]", "i")
-    res = run("sudo pacman -S haveged")
-    if res == 0:
-        log("Пакет оптимизации успешно установлен. [Шаг 2]", "c")
-        log("Запуск службы.", "i")
-        res = run("sudo systemctl enable haveged")
-        if res == 0:
-            log("Служба оптимизации успешно запущена. [Шаг 2]", "c")
-        else:
-            log("Служба оптимизации не запущена. [Шаг 2]", "e")
-    else:
-        log("Пакет оптимизации не установлен. [Шаг 2]", "e")
+def vpn():
+    os.system("mkdir -p /home/nironic/AutoStart/vpn")
     
-    log("Установка пакетов для оптимизации системы. [Шаг 3]", "i")
-    res = run("sudo pacman -S cpupower")
-    if res == 0:
-        log("Пакет оптимизации успешно установлен. [Шаг 2]", "c")
-        log("Запуск службы.", "i")
-        #run("echo governor=\"performance\" >> /etc/default/cpupower")
-        run("sudo cpupower frequency-set -g performance")
-        res = run("sudo systemctl enable cpupower")
-        if res == 0:
-            log("Служба оптимизации успешно запущена. [Шаг 2]", "c")
-        else:
-            log("Служба оптимизации не запущена. [Шаг 2]", "e")
-    else:
-        log("Пакет оптимизации не установлен. [Шаг 2]", "e")
-    
-    
+    start = """
+#!/bin/bash
+/usr/bin/sslocal -c /home/nironic/AutoStart/vpn/profile.json
+    """
 
-log("Модули импортированы.", "c")
-pacmans()
-log("Обновление системы..", "i")
-res = run("sudo pacman -Syu")
-if res == 0:
-    log("Обновление системы успешно завершена.", "c")
-else:
-    log("Обновление системы не произведено.", "e")
-log("Запуск установки Bluez и Bluez-utils (Для полноценной работы bluetooth драйвера)", "i")
-install(["bluez", "bluez-utils"])
-log("Настройка и запуск bluetooth.", "i")
-res = run("systemctl start bluetooth.service")
-if res == 0:
-    log("Bluetooth настроен и запущен.", "c")
-else:
-    log("Bluetooth не настроен. Запуск не может быть произведён.")
-Gchrome()
-teleinstall()
-setting_pip()
-optimizate()
-log("Установка ПО завершена.", "c")
+    units= """
+[Unit]
+Description=Vpn
+After=default.target
+
+[Service]
+ExecStart=/home/nironic/AutoStart/vpn/start.sh
+Restart=always  # Перезапускать при сбое
+
+[Install]
+WantedBy=default.target
+    """
+    profile = """
+{
+    "server": "89.46.238.103",
+    "server_port": 443,
+    "local_port": 1080,
+    "password": "1IlfUya1mXeyVAbMmurzzq",
+    "method": "chacha20-ietf-poly1305"
+}
+    """
+    st = open("/home/nironic/AutoStart/vpn/start.sh", "w")
+    st.write(start)
+    st.close()
+    os.system("mkdir -p /home/nironic/.config/systemd/user")
+    unit = open("/home/nironic/.config/systemd/user/vpn.service", "w")
+    unit.write(units)
+    unit.close()
+    pr = open("/home/nironic/AutoStart/vpn/profile.json", "w")
+    pr.write(profile)
+    pr.close()
+    os.system("systemctl --user enable vpn.service")
+
+__install__()
+__install__yay__()
+for i in packet:
+    install(i)
+
+#Command
+#Trim
+def run(com):
+    os.system(com)
+
+command = """
+sudo systemctl enable fstrim.timer
+sudo systemctl enable bluetooth
+sudo systemctl enable --now ananicy
+sudo fstrim -v /
+"""
+for i in command.split("\n"):
+    run(i)
+
+vpn()
