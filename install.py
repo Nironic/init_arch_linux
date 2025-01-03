dac06bfd43ed9c0c6140dcf10ce3a975f7f21a31
Nironic <40025622+Nironic@users.noreply.github.com>
2025-01-03 12:45:29 +0000

    Update install.py

--- install.py
+++ install.py
@@ -1,79 +1,61 @@
 import os
 
-def __install__():
+def install_yay():
     os.system("sudo pacman -Syuu --noconfirm")
-    os.system("sudo pacman -Syuu python python-pip --noconfirm")
-    os.system("pip install colorama --break-system-packages")
-
-def __install__yay__():
     os.system("sudo pacman -S --needed base-devel git --noconfirm")
+    os.system("git clone git clone https://aur.archlinux.org/yay.git")
     os.system("cd yay && makepkg -si --noconfirm")
     os.system("sudo yay -Syuu --noconfirm")
 
 def install(pack):
-    os.system("sudo yay -Syuu --noconfirm " + pack)
+    os.system("yay -Syuu --noconfirm --needed " + pack)
+
+def hyprland():
+    os.system("cp -r hypr ~/.config/")
+
+def locale():
+    tm = "ru_RU.UTF-8 UTF-8"
+    file1 = open("/etc/locale.gen", "r")
+    data = file1.read()
+    file1.close()
+    data = data + "\n" + tm
+    file1 = open("/etc/locale.gen", "w")
+    file1.write(data)
+    file1.close()
+    os.system("sudo locale-gen")
+
+def install_theme():
+    os.system("git clone https://github.com/vinceliuice/Graphite-gtk-theme")
+    os.system("cd Graphite-gtk-theme && ./install")
 
 
-packet = ["ananicy", "google-chrome", "portproton", "visual-studio-code-bin", "mesa", "lib32-mesa", "vulkan-radeon", "lib32-vulkan-radeon", "vulkan-icd-loader", "lib32-vulkan-icd-loader", "vlc", "ardour", "telegram-desktop-bin", "steam", "bluez", "bluez-deprecated-tools", "shadowsocks"]
-
-def vpn():
-    os.system("mkdir -p /home/nironic/AutoStart/vpn")
-    
-    start = """
-#!/bin/bash
-/usr/bin/sslocal -c /home/nironic/AutoStart/vpn/profile.json
-    """
-
-    units= """
-[Unit]
-Description=Vpn
-After=default.target
-
-[Service]
-ExecStart=/home/nironic/AutoStart/vpn/start.sh
-Restart=always  # Перезапускать при сбое
-
-[Install]
-WantedBy=default.target
-    """
-    profile = """
-{
-    "server": "89.46.238.103",
-    "server_port": 443,
-    "local_port": 1080,
-    "password": "1IlfUya1mXeyVAbMmurzzq",
-    "method": "chacha20-ietf-poly1305"
-}
-    """
-    st = open("/home/nironic/AutoStart/vpn/start.sh", "w")
-    st.write(start)
-    st.close()
-    os.system("mkdir -p /home/nironic/.config/systemd/user")
-    unit = open("/home/nironic/.config/systemd/user/vpn.service", "w")
-    unit.write(units)
-    unit.close()
-    pr = open("/home/nironic/AutoStart/vpn/profile.json", "w")
-    pr.write(profile)
-    pr.close()
-    os.system("systemctl --user enable vpn.service")
-
-__install__()
-__install__yay__()
-for i in packet:
-    install(i)
-
-#Command
-#Trim
-def run(com):
-    os.system(com)
+def fish():
+    fl = open("~/.config/fish/config.fish", "w")
+    fl.write("if status is-interactive\n    export XDG_CURRENT_DESKTOP=Hyprland\n   export XDG_SESSION_TYPE=wayland\n   fastfetch\nend")
+    fl.close()
+    print("/bin/fish")
+    os.system("chsh")
 
 command = """
 sudo systemctl enable fstrim.timer
 sudo systemctl enable bluetooth
 sudo systemctl enable --now ananicy
-sudo fstrim -v /
+sudo systemctl enable sddm
 """
-for i in command.split("\n"):
-    run(i)
 
-vpn()
+install_yay()
+fl = open("packet.data", "r")
+data = fl.read()
+fl.close()
+
+for i in data.split("\n"):
+    if i != "":
+        install(i)
+
+hyprland()
+locale()
+install_theme()
+fish()
+
+for i in command.split("\n"):
+    os.system(i)
